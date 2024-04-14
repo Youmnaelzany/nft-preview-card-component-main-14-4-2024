dada73cc8bb558957f5cad4f4322d267cf072878
Youmna Ahmed <107641709+Youmnaelzany@users.noreply.github.com>
2024-04-14 04:21:51 +0200

    setting up the working space
    add Astro, React & Tailwind
    add folders & files
    add bun
    work with mobile first method
    finish the project

---
+++ tailwind.config.mjs
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: ["class"],
+  content: [
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./app/**/*.{ts,tsx}",
+    "./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}",
+  ],
+  prefix: "",
+  theme: {
+    fontFamily: {
+      Outfit: ["Outfit", "sans-serif"],
+    },
+    extend: {},
+  },
+  plugins: [require("tailwindcss-animate")],
+};
